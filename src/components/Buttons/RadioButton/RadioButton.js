0a379cb630b2e1002656d103d23bbdc23cab91d0
nitin devatraj <nitin.devatraj@chaabi.co>
2023-08-26 18:09:54 +0530

    add dark theme support to CheckBox, RadioButton, ToggleButton  components

--- src/components/Buttons/RadioButton/RadioButton.js
+++ src/components/Buttons/RadioButton/RadioButton.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import styles from "./RadioButton.module.scss";
 import { ReactComponent as RadioButtonIcon } from "../../../assets/icons/button-icons/radio-button-icon.svg";
+import { useSelector } from "react-redux";
 
 function RadioButton() {
   const [isRadioBtnClicked, setIsRadioBtnClicked] = useState(false);
+  const isDarkMode = useSelector((state) => state.theme.darkMode);
 
   function radioBtnHandler() {
     setIsRadioBtnClicked((prevState) => !prevState);
   }
   return (
-    <div className={styles.radioBtn} onClick={radioBtnHandler}>
+    <div
+      className={
+        isDarkMode ? styles.radioBtnDarkTheme : styles.radioBtnLightTheme
+      }
+      onClick={radioBtnHandler}
+    >
       {isRadioBtnClicked && <RadioButtonIcon className={styles.radioBtnIcon} />}
     </div>
   );
--- src/components/Buttons/ToggleButton/ToggleButton.js
+++ src/components/Buttons/ToggleButton/ToggleButton.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import styles from "./ToggleButton.module.scss";
 import { ReactComponent as ToggleButtonIcon } from "../../../assets/icons/button-icons/toggle-button-icon.svg";
+import { useSelector } from "react-redux";
 
 function ToggleButton({ onClick }) {
   const [isToggleActive, setIsToggleActive] = useState(false);
+  const isDarkMode = useSelector((state) => state.theme.darkMode);
 
   function toggleBtnHandler() {
     setIsToggleActive((prevState) => !prevState);
@@ -12,9 +14,9 @@ function ToggleButton({ onClick }) {
 
   return (
     <div
-      className={`${styles.toggleBtnInactive} ${
-        isToggleActive ? styles.toggleBtnActive : null
-      }`}
+      className={`${
+        isDarkMode ? styles.toggleBtnDarkTheme : styles.toggleBtnLightTheme
+      } ${isToggleActive ? styles.toggleBtnActive : null}`}
       onClick={toggleBtnHandler}
     >
       <ToggleButtonIcon className={styles.toggleIcon} />
