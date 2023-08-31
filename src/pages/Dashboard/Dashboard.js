8dd3f6473d36ada67f4f04892fd2cbd895ada523
Jyoti-KM <jyoti.kumari@chaabi.co>
2023-08-31 18:28:02 +0530

    Dashboard responsive is fixed

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,7 +2,16 @@ import React from "react";
 import styles from "./Dashboard.module.scss";
 
 function Dashboard() {
-  return <div className={styles.dashboardContainer}>Dashboard</div>;
+  return <div className={styles.dashboardContainer}>
+    <div className={styles.div1}></div>
+    <div className={styles.div2}></div>
+    <div className={styles.div3}></div>
+    <div className={styles.div4}></div>
+    <div className={styles.div5}></div>
+    <div className={styles.div6}></div>
+    <div className={styles.div7}></div>
+    <div className={styles.div8}></div>
+  </div>;
 }
 
 export default Dashboard;
